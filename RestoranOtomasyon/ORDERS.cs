3812dce6dc53c141ffe225f46dfd4f31b97957c4
Karahan Güllü <92548316+krhnqll@users.noreply.github.com>
2024-08-21 09:37:03 +0300

    Update ORDERS.cs

--- RestoranOtomasyon/ORDERS.cs
+++ RestoranOtomasyon/ORDERS.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -36,14 +36,14 @@ namespace RestoranOtomasyon
             try
             {
 
-                string queery = @"SELECT Orders.Date,Orders.Order_id, Bar.Bar_name, Desks.Desk_name, Kitchen.Kitchen_name,[User].User_name, [dbo].[Products].Prod_name
+                string queery = @"SELECT DISTINCT Orders.Order_id, Orders.Date,[dbo].[DD].Desk_name, [User].User_name, [dbo].[Products].Prod_name,[dbo].[Products].Prod_price, Bar.Bar_name, Kitchen.Kitchen_name 
                         FROM 
                         [dbo].[Orders]
                         JOIN [dbo].[Bar] ON [dbo].[Orders].Bar_id = [dbo].[Bar].Bar_id
                         JOIN [dbo].[Kitchen] ON [dbo].[Orders].Kitchen_id =  [dbo].[Kitchen].Kitchen_id
                         JOIN [dbo].[User] ON [dbo].[Orders].[User_id] = [dbo].[User].User_id
-                        JOIN [dbo].[Desks] ON [dbo].[Orders].Desk_id = [dbo].[Desks].Desk_id
-                        JOIN [dbo].[Products] ON [dbo].[Orders].Prod_id = [dbo].[Products].Prod_id ";
+                        JOIN [dbo].[Products] ON [dbo].[Orders].Prod_id = [dbo].[Products].Prod_id 
+						JOIN [dbo].[DD] ON [dbo].[Orders].Desk_id = [dbo].[DD].Desk_id ";
 
                 SqlCommand ordList = new SqlCommand(queery, db.SqlServer);
                 SqlDataAdapter rd = new SqlDataAdapter(ordList);
@@ -65,34 +65,7 @@ namespace RestoranOtomasyon
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
-            {
-
-                string queery = @"SELECT Orders.Date,Orders.Order_id, Bar.Bar_name, Desks.Desk_name, Kitchen.Kitchen_name,[User].User_name, [dbo].[Products].Prod_name
-                        FROM 
-            [dbo].[Orders]
-            JOIN [dbo].[Bar] ON [dbo].[Orders].Bar_id = [dbo].[Bar].Bar_id
-            JOIN [dbo].[Kitchen] ON [dbo].[Orders].Kitchen_id =  [dbo].[Kitchen].Kitchen_id
-            JOIN [dbo].[User] ON [dbo].[Orders].[User_id] = [dbo].[User].User_id
-            JOIN [dbo].[Desks] ON [dbo].[Orders].Desk_id = [dbo].[Desks].Desk_id
-            JOIN [dbo].[Products] ON [dbo].[Orders].Prod_id = [dbo].[Products].Prod_id ";
-
-                SqlCommand ordList = new SqlCommand(queery, db.SqlServer);
-                SqlDataAdapter rd = new SqlDataAdapter(ordList);
-                DataTable Ord = new DataTable();
-                rd.Fill(Ord);
-
-                dataGridView1.DataSource = Ord;
-
-            }
-            catch
-            {
-
-            }
-            finally
-            {
-
-            }
+            FetchDataFromOrdersTable();
         }
     }
 }
